116b41329b9610ec355cbecc55cff097ff96073e
Kelsey Brennan <brennank@mail.gvsu.edu>
2017-11-06 13:28:18 -0500

    Add GridView to each tab in store/inventory.

--- app/src/main/java/cisgvsu/biotowerdefense/InventoryFragment.java
+++ app/src/main/java/cisgvsu/biotowerdefense/InventoryFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Kelsey on 11/5/2017.
@@ -12,6 +16,20 @@ import android.view.ViewGroup;
 public class InventoryFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.inventory_fragment, container, false);
+        // Get the view for this fragment
+        View view = inflater.inflate(R.layout.inventory_fragment, container, false);
+
+        // Set up adapter
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Tower 1");
+        list.add("Tower 2");
+        list.add("Tower 3");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, list);
+
+        // Attach to GridView
+        GridView gridView = (GridView) view.findViewById(R.id.inventoryGridView);
+        gridView.setAdapter(adapter);
+
+        return view;
     }
 }
